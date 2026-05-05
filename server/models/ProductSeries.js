71c93811f27c9765f8b4a7a57ea1048dcb0ff920
Jason Gu <king.gs1314@outlook.com>
2026-05-06 01:27:18 +0800

    refactor(model): drop beadCategory, add products[] to ProductSeries

--- server/models/ProductSeries.js
+++ server/models/ProductSeries.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const productSeriesSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, default: '' },
-  beadCategory: { type: String, default: '' },
   image: { type: String, default: '' },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
 }, { timestamps: true });
 
 module.exports = mongoose.model('ProductSeries', productSeriesSchema);
